66a136b6112a8d5663015933294462cd6cdca227
Alexander Duong <a9duong@uwaterloo.ca>
2016-01-20 21:31:22 -0500

    Update January 20, 9:31
    Added a main menu

---
+++ Pong 2015/PongClone/PongClone/main_game.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "game_state.h"
+
+class main_game : public tiny_state
+{
+public:
+	virtual void Initialize(sf::RenderWindow* window);
+	virtual void Update(sf::RenderWindow* window);
+	virtual void Render(sf::RenderWindow* window);
+	virtual void Destroy(sf::RenderWindow* window);
+
+private:
+	sf::Font* font;
+	sf::Text* title;
+	sf::Text* play;
+	sf::Text* quit;
+
+	int selected;
+
+	bool upKey, downKey;
+};
